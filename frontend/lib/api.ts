a0d67ed08c91d61600aecf971f6f994df21811fd
tvaldig <timotiusvivaldi@gmail.com>
2026-01-04 23:33:26 +0700

    fix(api): move to env

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "https://talentgrowth-test-production.up.railway.app/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 })
 
 api.interceptors.request.use((config) => {
