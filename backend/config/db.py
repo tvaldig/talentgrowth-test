dd47fcf5d356e51b631a45cd6f35e2e95cc213c7
tvaldig <timotiusvivaldi@gmail.com>
2026-01-04 14:46:11 +0700

    refactor(db): db instance

--- backend/config/db.py
+++ backend/config/db.py
@@ -1,32 +1,28 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
-import psycopg2
 
 load_dotenv()
 
-DATABASE_USER = os.getenv("DATABASE_USER")
-DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
-DATABASE_HOST = os.getenv("DATABASE_HOST")
-DATABASE_PORT = os.getenv("DATABASE_PORT")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-DATABASE_SSLMODE = os.getenv("DATABASE_SSLMODE")
-
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-# SQLAlchemy setup
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,  
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine,
+)
+
 Base = declarative_base()
 
-# psycopg2 connection
-cnx = psycopg2.connect(
-    user=DATABASE_USER, 
-    password=DATABASE_PASSWORD,
-    host=DATABASE_HOST, 
-    port=DATABASE_PORT, 
-    database=DATABASE_NAME,
-    sslmode=DATABASE_SSLMODE  
-)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
