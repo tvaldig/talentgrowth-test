185f78618c5a6879dbd461328ca684dea916f3e6
tvaldig <timotiusvivaldi@gmail.com>
2026-01-04 18:48:38 +0700

    feat(api): change to prod

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://talentgrowth-test-production.up.railway.app/",
 })
 
 api.interceptors.request.use((config) => {
